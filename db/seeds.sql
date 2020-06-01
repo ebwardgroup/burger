db863fc22c5a71cd2aa4fd0d03363a02d79e058c
Eric Ward <ebwardgroup@gmail.com>
2020-05-31 23:33:27 -0600

    submitting

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pizza Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BLT Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Ham and Egg Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Burger', FALSE);
